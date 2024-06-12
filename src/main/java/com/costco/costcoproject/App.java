1c6063c895c2423d1a92ccae5a3b0d8449e0ae1f
Zaneta <zaneta.hanna@gmail.com>
2024-06-11 22:01:54 -0500

    new commit

---
+++ src/main/java/com/costco/costcoproject/App.java
@@ -0,0 +1,13 @@
+package com.costco.costcoproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
